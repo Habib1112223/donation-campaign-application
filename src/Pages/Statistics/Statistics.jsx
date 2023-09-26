b29a8c47d5e4dd7f834a85530c08d7577b5ccd86
Habibur1112223 <webdevelopment.habib2023@gmail.com>
2023-09-26 09:29:44 +0600

    the header session code has finished

---
+++ src/Pages/Statistics/Statistics.jsx
@@ -0,0 +1,11 @@
+
+
+const Statistics = () => {
+      return (
+            <div>
+                 This is a Statistics 
+            </div>
+      );
+};
+
+export default Statistics;
---
+++ src/components/Header/Navbar/Logo/Logo.jsx
@@ -0,0 +1,12 @@
+
+
+const Logo = () => {
+      return (
+            <div>
+                 <h1 className="text-2xl font-bold">Donation</h1>
+                 <img src="Logo.png" alt="" /> 
+            </div>
+      );
+};
+
+export default Logo;
