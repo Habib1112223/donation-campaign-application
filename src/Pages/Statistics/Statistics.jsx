ef7a34d5badd274490a121e7a1dd8036d8a8f9cb
Habibur1112223 <webdevelopment.habib2023@gmail.com>
2023-09-27 17:53:54 +0600

    correction the statistics session

--- src/Pages/Statistics/Statistics.jsx
+++ src/Pages/Statistics/Statistics.jsx
@@ -1,20 +1,23 @@
-// import Chart from "react-google-charts";
+
 import { Chart } from "react-google-charts";
 
 
 const Statistics = () => {
 
       const data = [
+            // ["Task", "Hours per Day"],
+            // ["Work", 11],
+            // ["Eat", 2],
+            // ["Commute", 2],
+            // ["Watch TV", 2],
+            // ["Sleep", 7], // CSS-style declaration
             ["Task", "Hours per Day"],
-            ["Work", 11],
-            ["Eat", 2],
-            ["Commute", 2],
-            ["Watch TV", 2],
-            ["Sleep", 7], // CSS-style declaration
+             ["Your Donation", 33.3],
+             ["Total Donation", 67.7],
       ];
     
     const options = {
-      title: "My Daily Activities",
+      title: "Donation Activities",
       pieHole: 0.4,
       is3D: false,
     };
